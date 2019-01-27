09526f5d542bbaf1065cb7b9e9bd03c12584dab7
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:15:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {"Cool Alyx": "Funky Town"}
 function updatePlaylist(obj, artistName, songTitle) {
     return obj[artistName] = songTitle;
 }
+
+function removeFromPlaylist(obj, artistName) {
+    return obj.delete(artistName)
+}
